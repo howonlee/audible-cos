36c52ea4417c3777044597886868644fc8c71908
howon lee <hlee.howon@gmail.com>
2019-10-26 09:08:23 -0700

    add that argparse

--- sound.py
+++ sound.py
@@ -7,7 +7,6 @@ import argparse
 
 FREQ = 44100
 NUM_SECS = 10
-MIN_VAL = 0
 MAX_VAL = 128
 PROBLEM_CARD = 7
 LINSPACE_SCALE = int(1e2)
@@ -19,16 +18,23 @@ def prod(iterable):
 def scaled_cos(scale, inp):
     return np.cos(scale * inp)
 
-def gen_problem(min_val, max_val, problem_card):
-    return list(npr.randint(min_val, max_val, problem_card))
+def gen_problem(max_val, problem_card):
+    return list(npr.randint(0, max_val, problem_card))
 
-def gen_sound(problem, linspace_scale, freq, num_secs, vol_adj):
-    inp = np.linspace(0, linspace_scale, freq * num_secs)
+def gen_sound(problem, linspace_stop, freq, num_secs, vol_adj):
+    inp = np.linspace(0, linspace_stop, freq * num_secs)
     curr_res = prod(scaled_cos(member, inp) for member in problem)
     return curr_res * vol_adj
 
 if __name__ == "__main__":
-    pass
-
-
-sd.play(curr_res, FREQ, blocking=True)
+    parser = argparse.ArgumentParser(description="Play sounds corresponding to an NP-complete problem, or, alternately phrased, with very many and very high overtones.")
+    parser.add_argument('--freq', type=int, default=44100, help='frequency')
+    parser.add_argument('--num_secs', type=int, default=10, help='number of seconds of sound')
+    parser.add_argument('--max_val', type=int, default=128, help='maximum number for random number partition instance')
+    parser.add_argument('--problem_card', type=int, default=7, help='cardinality for number partition instance')
+    parser.add_argument('--linspace_stop', type=int, default=int(1e2), help='how far the linear space transformed into cos scales')
+    parser.add_argument('--vol_adj', type=int, default=int(1e2), help='adjustment for volume scaling')
+    args = parser.parse_args()
+    prob = gen_problem(args.max_val, args.problem_card)
+    sound = gen_sound(prob, args.linspace_stop, args.freq, args.num_secs, args.vol_adj)
+    sd.play(sound, FREQ, blocking=True)

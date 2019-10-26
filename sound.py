60e7f88d37abd8b819f0e4dc92c8f5d27a1a2e09
howon lee <hlee.howon@gmail.com>
2019-10-25 22:41:03 -0700

    interesting params findable, I think I will start w/ 5 interesting sets

--- sound.py
+++ sound.py
@@ -7,19 +7,17 @@ import functools
 def prod(iterable):
     return functools.reduce(operator.mul, iterable, 1)
 
-def scaled_cos(scale, amp, inp):
-    return amp * np.cos(scale * inp)
+def scaled_cos(scale, inp):
+    return np.cos(scale * inp)
 
-# var1 = top of randint
-# var2 = number genned
-freqs = list(npr.randint(0, 1e3, 8))
-amps = list((npr.rand(30) - 0.5) * 2)
-problem = zip(freqs, amps)
+# var1 = maxval of randint
+# var2 = number of ints genned
+problem = list(npr.randint(0, 512, 9))
 
 fs = 44100
 secs = 50
 # var3 = linspace
-inp = np.linspace(0, 1e6, fs * secs)
-curr_res = prod(scaled_cos(member, amp, inp) for member, amp in problem)
-curr_res /= (curr_res.std() / 1e1)
+inp = np.linspace(0, 1e2, fs * secs)
+curr_res = prod(scaled_cos(member, inp) for member in problem)
+curr_res = curr_res * 1e2
 sd.play(curr_res, fs, blocking=True)
